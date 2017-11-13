ff7aa94558982ca96cbfb451d3af98135918f1db
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-11-13 19:40:20 +0200

    am inceput device motion

--- js/senzori.js
+++ js/senzori.js
@@ -1,5 +1,9 @@
 
 window.addEventListener("deviceorientation",on_device_orientation);
+window.addEventListener("devicemotion",on_device_motion);
+
+///---------------
+
 
 function on_device_orientation(e)
 {
@@ -8,4 +12,11 @@ function on_device_orientation(e)
 	        document.getElementById("id_y").innerHTML="y="+e.gamma;
 
 
+}
+
+function on_device_motion(e)
+{
+	
+	
+	
 }
