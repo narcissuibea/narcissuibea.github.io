8b77db1c978c358efbb6693c34c51ac4238348ac
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-11-13 19:12:46 +0200

    am citit senzorii de orientare

--- js/senzori.js
+++ js/senzori.js
@@ -1,1 +1,11 @@
-document.getElementById("id_x").innerHTML="Salut";
+
+window.addEventListener("deviceorientation",on_device_orientation);
+
+function on_device_orientation(e)
+{
+	document.getElementById("id_z").innerHTML="e.alpha";
+		document.getElementById("id_x").innerHTML="e.beta";
+	        document.getElementById("id_y").innerHTML="e.gamma";
+
+
+}
