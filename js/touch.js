bef4d9bae608181b74df53a361a600d68fcc221a
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-12-04 19:49:30 +0200

    culori

--- js/touch.js
+++ js/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.1";
 
 var canvas =document.getElementById("id_canvas");
 var context =canvas.getContext("2d");
@@ -14,7 +14,7 @@ function on_start_touch(e)
 	for (var i=0; i<touches.length; i++){
 		context.beginPath();
 		context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI );	
-		context.strokeStyle=;lista_culori[i];
+		context.strokeStyle=lista_culori[i];
 		context.stroke();
 		
 		
