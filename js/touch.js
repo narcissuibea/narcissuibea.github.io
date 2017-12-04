a88514e1cf5ed37861d31207c6afdbfb39403754
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-12-04 19:45:36 +0200

    listaculori

--- js/touch.js
+++ js/touch.js
@@ -3,8 +3,8 @@ document.getElementById("id_business_version").innerHTML = "Business version = 2
 var canvas =document.getElementById("id_canvas");
 var context =canvas.getContext("2d");
 canvas.addEventListener("touchstart", on_start_touch);
-
-//----------------------------------------
+var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FFFFFF"];
+//----------------------------------------1
 
 function on_start_touch(e)
 {
@@ -14,7 +14,10 @@ function on_start_touch(e)
 	for (var i=0; i<touches.length; i++){
 		context.beginPath();
 		context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI );	
+		context.strokeStyle=;lista_culori[i];
 		context.stroke();
+		
+		
 	}
 	
 }
