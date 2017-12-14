be03da85c8d0833e7230458dd932fcf2b5b9adab
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-12-14 18:35:03 +0200

    on_move

--- js/touch.js
+++ js/touch.js
@@ -1,8 +1,12 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.2";
 
 var canvas =document.getElementById("id_canvas");
 var context =canvas.getContext("2d");
+
+
 canvas.addEventListener("touchstart", on_start_touch);
+canvas.addEventListener("touchstart", on_move_touch);
+
 var lista_culori=["#FF0000","#00FF00","#0000FF","#FFFF00","#FFFFFF"];
 //----------------------------------------1
 
@@ -10,7 +14,7 @@ function on_start_touch(e)
 {
 	e.changedTouches;
 	
-	var touches = e.changedTouches;
+	var touches = e.touches;
 	for (var i=0; i<touches.length; i++){
 		context.beginPath();
 		context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI );	
@@ -22,3 +26,17 @@ function on_start_touch(e)
 	
 }
 //----------------------------------------
+
+function on_move_touch(e)
+{
+	e.changedTouches;
+	
+	var touches = e.touches;
+	for (var i=0; i<touches.length; i++){
+		context.beginPath();
+		context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI );	
+		context.strokeStyle=lista_culori[i];
+		context.stroke();
+
+
+
