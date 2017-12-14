8da27a35886b27636ce8211e58dd2284d49f8e88
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2017-12-14 19:22:35 +0200

    accuracy

--- js/gps.js
+++ js/gps.js
@@ -1,12 +1,13 @@
 
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.1";
 
 navigator.geolocation.getCurentPosition(function on_position_success, function on_position_failure);
 //--------------------------------------
 function on_position_success(e)
 {
-	document.getElementById("id_lat").innerHTML=e.coords.latitude;
-		document.getElementById("id_long").innerHTML=e.coords.longitude;
+	document.getElementById("id_lat").innerHTML="Latitude=+"e.coords.latitude;
+		document.getElementById("id_long").innerHTML="Longitude=+"e.coords.longitude;
+		     document.getElementById("id_acc").innerHTML="Accuracy=+"e.coords.accuracy + "m";
 
 }
 
