8048920a6120a8d653764cc0400a4dbe0ba8b8c9
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-15 19:02:42 +0200

    speech

---
+++ js/speech.js
@@ -0,0 +1,28 @@
+var d = new Date();
+
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+//------------------------------------
+
+var speech=new SpeechRecognition();
+speech.onresults= on_speech_results;
+speech.onspeechend=on_speech_end;
+speech.lang="en-US";
+//-----------------------------------
+function recognize()
+{
+	speech.start();
+}
+
+//--------------------------------------
+function on_speech_end(e)
+{
+	speech.stop();
+}
+
+//--------------------------------------
+function on_speech_results(e)
+{
+	document.getElementById ("id_speech").innerHtml=e.results.item(0).item(0).transcript;
+	
+}
