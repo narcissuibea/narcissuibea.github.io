6181b0047d9cd3843341e3946f981089f32abe13
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-15 19:06:58 +0200

    speech2

--- js/speech.js
+++ js/speech.js
@@ -4,7 +4,7 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 
 //------------------------------------
 
-var speech=new SpeechRecognition();
+var speech=new webkitSpeechRecognition();
 speech.onresults= on_speech_results;
 speech.onspeechend=on_speech_end;
 speech.lang="en-US";
