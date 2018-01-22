d6c1d5eae28a296142f2973e52f2226a2a447e4e
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-22 18:59:44 +0200

    vibrate3

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,7 +1,7 @@
 
 var d = new Date();
 
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 
 var img= document.getElementById("id_img");
 img.addEventListener("touchstart", on_touch_start);
@@ -11,7 +11,7 @@ img.addEventListener("touchstart", on_touch_start);
 function on_touch_start()
 
 {
-window.navigator.vibrate(200); 
+window.navigator.vibrate([100, 100, 200, 200, 300, 300); 
 
 }
 
