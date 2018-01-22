aea0cefae19207d2be4be522be77cef31a48743f
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-22 19:45:35 +0200

    light0

---
+++ js/light.js
@@ -0,0 +1,28 @@
+var d = new Date();
+
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0";
+
+
+var sensor= new AmbientalLightSensor();
+sensor.onreading= on_light_reading;
+sensor.onerror= on_light_error;
+sensor.start();
+
+//---------------------------------------------
+function  on_light_reading()
+
+{
+	document.getElementById("id_light").innerHTML = sensor.illuminance;
+	
+}
+
+//-----------------------------------------------
+
+function on_light_error ()
+
+{
+	
+	alert("Cannot read light sensor");
+}
+
+//---------------------------------------------
