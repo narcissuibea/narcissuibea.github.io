dbedbbdc0d62865dc518e1f2a9a1d3fab712e145
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-22 17:49:53 +0200

    camera0

---
+++ js/cam.js
@@ -0,0 +1,32 @@
+var d = new Date();
+
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+//------------------------------------
+var constraints={audio: true, video: true};
+
+navigator.MediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
+
+
+var video= document.getElementById("id_video");
+
+//------------------------------------
+
+function on_cam(stream)
+
+{
+	video.srcObject = stream;
+	
+}
+
+//--------------------------------------
+
+function on_error(e)
+
+{
+	
+    alert("Error: cannot connect to camera!");	
+	
+}
+
+//-------------------------------------
