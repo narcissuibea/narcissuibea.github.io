bf6f553f4c642ed86e2ae5580e3201d50cc2df3a
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-22 19:21:20 +0200

    vibrate6stop

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,7 +1,7 @@
 
 var d = new Date();
 
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 
 var img= document.getElementById("id_img");
 img.addEventListener("touchstart", on_touch_start);
@@ -9,10 +9,10 @@ img.addEventListener("touchend", on_touch_end);
 
 //---------------------------------------------------
 
-function on_touch_start()
+function on_touch_start(e)
 
 {
-	
+	e.preventDefault();
 	timer_id= setInterval (start_vibrate, 100);
 	
 }
