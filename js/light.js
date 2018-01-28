3ffeeb63ccc41037076869e339ed8d5d416beccf
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-28 22:17:06 +0200

    light

--- js/light.js
+++ js/light.js
@@ -3,7 +3,7 @@ var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0";
 
 
-var sensor= new AmbientalLightSensor();
+var sensor= new AmbientLightSensor();
 sensor.onreading= on_light_reading;
 sensor.onerror= on_light_error;
 sensor.start();
