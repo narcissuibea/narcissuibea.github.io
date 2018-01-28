67c56b5f578bb95e77e5b833757f8470302ed504
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-28 22:06:59 +0200

    tema4

--- js/tema.js
+++ js/tema.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.2";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.3";
 
 //window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -6,8 +6,25 @@ window.addEventListener("devicemotion", on_device_motion);
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var image = document.getElementById('source');
-
-ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
 
 
+var image = new Image(60, 45);   // using optional size for image
+image.onload = drawImageActualSize; // draw when image has loaded
+
+// load an image of intrinsic size 300x227 in CSS pixels
+image.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
+
+function drawImageActualSize() {
+  // use the intrinsic size of image in CSS pixels for the canvas element
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
+
+  // will draw the image as 300x227 ignoring the custom size of 60x45
+  // given in the constructor
+  ctx.drawImage(this, 0, 0);
+
+  // To use the custom size we'll have to specify the scale parameters 
+  // using the element's width and height properties - lets draw one 
+  // on top in the corner:
+  ctx.drawImage(this, 0, 0, this.width, this.height);
+}
