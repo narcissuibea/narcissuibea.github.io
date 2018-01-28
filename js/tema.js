90ffde6026f5d60414720acf1272b3a79b60ad5b
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-28 22:44:33 +0200

    tema6

--- js/tema.js
+++ js/tema.js
@@ -1,6 +1,6 @@
 var d = new Date();
 
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 
 var img= document.getElementById("id_img");
 img.addEventListener("touchstart", on_touch_start);
