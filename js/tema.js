da11ac2a96ffc92dbda1a77dcdce385de37869be
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-28 22:42:31 +0200

    tema5

--- js/tema.js
+++ js/tema.js
@@ -1,30 +1,34 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.3";
+var d = new Date();
 
-//window.addEventListener("deviceorientation", on_device_orientation);
-window.addEventListener("devicemotion", on_device_motion);
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".2"; 
 
+var img= document.getElementById("id_img");
+img.addEventListener("touchstart", on_touch_start);
+img.addEventListener("touchend", on_touch_end);
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+//---------------------------------------------------
 
+function on_touch_start(e)
 
-var image = new Image(60, 45);   // using optional size for image
-image.onload = drawImageActualSize; // draw when image has loaded
-
-// load an image of intrinsic size 300x227 in CSS pixels
-image.src = 'https://mdn.mozillademos.org/files/5397/rhino.jpg';
-
-function drawImageActualSize() {
-  // use the intrinsic size of image in CSS pixels for the canvas element
-  canvas.width = this.naturalWidth;
-  canvas.height = this.naturalHeight;
-
-  // will draw the image as 300x227 ignoring the custom size of 60x45
-  // given in the constructor
-  ctx.drawImage(this, 0, 0);
-
-  // To use the custom size we'll have to specify the scale parameters 
-  // using the element's width and height properties - lets draw one 
-  // on top in the corner:
-  ctx.drawImage(this, 0, 0, this.width, this.height);
+{
+	e.preventDefault();
+	timer_id= setInterval (start_vibrate, 150);
+	
 }
+
+//----------------------------------------------------	
+	
+	function start_vibrate()
+	{
+    window.navigator.vibrate(200); 
+
+}
+//-----------------------------------------------------
+
+function on_touch_end(e)
+{
+	e.preventDefault();
+	clearInterval(timer_id);
+}
+
+//---------------------------------------------------------
