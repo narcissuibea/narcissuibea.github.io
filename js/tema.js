c62e85993d68d95427e6cab943d14357f0822c61
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-29 00:06:33 +0200

    temaaaa

--- js/tema.js
+++ js/tema.js
@@ -1,10 +1,9 @@
 var d = new Date();
 
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
-document.getElementById("demo").innerHTML = Date();
 //---------
 
 
 function displayDate() {
-    
+    document.getElementById("demo").innerHTML = Date();
 }
