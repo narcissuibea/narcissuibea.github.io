69d631ed2ab2915da3631a5ab46baaa124fe57f1
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-29 00:09:42 +0200

    tema22

--- js/tema.js
+++ js/tema.js
@@ -1,7 +1,3 @@
-var d = new Date();
-
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".1"; 
-//---------
 
 
 function displayDate() {
