23e794545d8851a371fb347452dfcc81e0514a4f
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-28 23:20:08 +0200

    tema7

--- js/tema.js
+++ js/tema.js
@@ -1,34 +1,48 @@
-var d = new Date();
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.9";
 
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+//window.addEventListener("deviceorientation", on_device_orientation);
+window.addEventListener("devicemotion", on_device_motion);
 
-var img= document.getElementById("id_img");
-img.addEventListener("touchstart", on_touch_start);
-img.addEventListener("touchend", on_touch_end);
+var canvas = document.getElementById("id_canvas");
+var ctx = canvas.getContext("2d");
 
-//---------------------------------------------------
 
-function on_touch_start(e)
+var unghiuri={gamma:0,beta:0};
 
+setInterval(deseneaza_cerc,60,unghiuri);
+
+
+//-----------------------------------------------------
+function deseneaza_cerc(u)
 {
-	e.preventDefault();
-	timer_id= setInterval (start_vibrate, 1050);
-	
-}
-
-//----------------------------------------------------	
-	
-	function start_vibrate()
-	{
-    window.navigator.vibrate(20); 
-
+		ctx.clearRect(0, 0, 600, 600);
+		
+		//ctx.rect(0, 0, 400, 400);
+		//ctx.stroke();
+		ctx.strokeRect(0,0,400,400);
+		
+		
+		ctx.beginPath();
+		ctx.arc(200 + u.gamma * 500 / 90, 200 + u.beta * 200 / 90, 20, 0, 2 * Math.PI);
+		ctx.fillStyle = "#FF0000";
+		//ctx.fill();
+		ctx.strokeStyle = "#00FF00";
+		ctx.lineWidth = 5;
+		ctx.stroke();
 }
 //-----------------------------------------------------
-
-function on_touch_end(e)
+function on_device_orientation(e)
 {
-	e.preventDefault();
-	clearInterval(timer_id);
+	//deseneaza_cerc(e.gamma, e.beta);
+	
+	unghiuri.gamma=e.gamma;
+	unghiuri.beta=e.beta;
 }
-
-//---------------------------------------------------------
+//-----------------------------------------------------
+function on_device_motion(e)
+{
+	 unghiuri.beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	 unghiuri.gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	
+	//deseneaza_cerc(gamma, beta);
+}
