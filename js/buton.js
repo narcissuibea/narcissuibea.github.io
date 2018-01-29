18cef74f6c8ef66fa1697eba0912c8a3c9b096ac
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-29 13:46:39 +0200

    05

--- js/buton.js
+++ js/buton.js
@@ -1,6 +1,6 @@
 var d = new Date();
 
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".77";
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".05";
 
 
  var x = document.getElementById("myBtn");
