3aea90dd1dcff4912d3c3e349bf1f07cd63962b1
narcissuibea <32843714+narcissuibea@users.noreply.github.com>
2018-01-29 13:10:43 +0200

    buton

---
+++ js/buton.js
@@ -0,0 +1,21 @@
+var d = new Date();
+
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".9";
+
+
+ var x = document.getElementById("myBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("click", mySecondFunction);
+x.addEventListener("mouseout", myThirdFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML += "Moused over!<br>";
+}
+
+function mySecondFunction() {
+    document.getElementById("demo").innerHTML += "Clicked!<br>";
+}
+
+function myThirdFunction() {
+    document.getElementById("demo").innerHTML += "Moused out!<br>";
+}
